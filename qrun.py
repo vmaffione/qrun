dc4c2767fa1415fc62d31dc3ba4ff87eaa806138
Vincenzo Maffione <v.maffione@gmail.com>
2017-03-31 09:48:12 +0200

    --dry-run: fix help string

--- qrun.py
+++ qrun.py
@@ -121,7 +121,7 @@ epilog = "2015 Vincenzo Maffione"
 argparser = argparse.ArgumentParser(description = description,
                                     epilog = epilog)
 argparser.add_argument('--dry-run', action='store_true',
-                       help = "Only show the built QEMU command line")
+                       help = "Only show the generated QEMU command line")
 argparser.add_argument('--install-from-iso',
                        help = "Path to the installation ISO", type = str,
                        default = '')
